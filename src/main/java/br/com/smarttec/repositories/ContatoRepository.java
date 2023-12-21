ca56cdce13e9409e470d4e1e0d18602338d781fa
pedrohamaranhao13 <pedrinhoelloco@outlook.com.br>
2023-12-21 19:53:13 -0300

    feat: ContatoRepository(create/update/delete/find)

--- src/main/java/br/com/smarttec/repositories/ContatoRepository.java
+++ src/main/java/br/com/smarttec/repositories/ContatoRepository.java
@@ -1,5 +1,113 @@
 package br.com.smarttec.repositories;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.smarttec.entites.Contato;
+import br.com.smarttec.factories.ConnectionFactory;
+
 public class ContatoRepository {
 
+	public void create(Contato contato) throws Exception {
+		
+		Connection connection = ConnectionFactory.getConnection();
+		
+		PreparedStatement statement = connection.prepareStatement("insert into conato(nome, telefone, email, observacoes, idusuario) values(?,?,?,?,?)");
+		statement.setString(1, contato.getNome());
+		statement.setString(2, contato.getTelefone());
+		statement.setString(3, contato.getEmail());
+		statement.setString(4, contato.getObservacoes());
+		statement.setInt(5, contato.getIdUsuario());
+		statement.execute();
+		
+		connection.close();
+	}
+	
+	public void update(Contato contato) throws Exception {
+		
+		Connection connection = ConnectionFactory.getConnection();
+		
+		PreparedStatement statement = connection.prepareStatement("update conatonome=?, telefone=?, email=?, observacoes=?  where idcontato =? ");
+		statement.setString(1, contato.getNome());
+		statement.setString(2, contato.getTelefone());
+		statement.setString(3, contato.getEmail());
+		statement.setString(4, contato.getObservacoes());
+		statement.setInt(5, contato.getIdContato());
+		statement.execute();
+		
+		connection.close();
+	}
+	
+	public void delete(Contato contato) throws Exception {
+		
+		Connection connection = ConnectionFactory.getConnection();
+		
+		PreparedStatement statement = connection.prepareStatement("delete from conato  where idcontato =?");
+		statement.setInt(1, contato.getIdContato());
+		statement.execute();
+		
+		connection.close();
+	}
+	
+	public List<Contato> findByUsuario(Integer idUsuario) throws Exception {
+		
+		Connection connection = ConnectionFactory.getConnection();
+		
+		PreparedStatement statement = connection.prepareStatement("select * from contato where  idusuario = ?");
+		
+		statement.setInt(1, idUsuario);
+		ResultSet resultSet = statement.executeQuery();
+		
+		List<Contato> lista = new ArrayList<Contato>();
+		
+		while(resultSet.next()) {
+			
+			Contato contato = new Contato();
+			
+			contato.setIdContato(resultSet.getInt("idcontato"));
+			contato.setNome(resultSet.getString("nome"));
+			contato.setEmail(resultSet.getString("email"));
+			contato.setTelefone(resultSet.getString("telefone"));
+			contato.setObservacoes(resultSet.getString("observacoes"));
+			contato.setIdUsuario(resultSet.getInt("idusuario"));
+			
+			lista.add(contato);
+		}
+		
+		connection.close();
+		return lista;
+	}
+	
+	
+		public Contato findById(Integer idContato) throws Exception {
+		
+		Connection connection = ConnectionFactory.getConnection();
+		
+		PreparedStatement statement = connection.prepareStatement("select * from contato where  idcontato = ?");
+		
+		statement.setInt(1, idContato);
+		ResultSet resultSet = statement.executeQuery();
+		
+		Contato contato = null;
+		
+		if(resultSet.next()) {
+			
+			contato = new Contato();
+		
+			contato.setIdContato(resultSet.getInt("idcontato"));
+			contato.setNome(resultSet.getString("nome"));
+			contato.setEmail(resultSet.getString("email"));
+			contato.setTelefone(resultSet.getString("telefone"));
+			contato.setObservacoes(resultSet.getString("observacoes"));
+			contato.setIdUsuario(resultSet.getInt("idusuario"));
+		}
+		
+		connection.close();
+		return contato;
+	
+	}
+	
 }
